cbdf0911006f1eeed14b25594b5f3cc3221a6dc3
Eshwar-B <1728eshwar@gmail.com>
2025-05-04 13:46:54 +0530

    API Gateway Implemented

--- Service-01-welcome/src/main/java/in/ashokit/restControllers/WelcomeRestController.java
+++ Service-01-welcome/src/main/java/in/ashokit/restControllers/WelcomeRestController.java
@@ -1,15 +1,21 @@
 package in.ashokit.restControllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeRestController {
 
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/welcome")
 	public String getWelcomeMsg()
 	{
-		return "Welcome to Micro Services";
+		String msg = "Welcome to Micro Services ! , Port number : " + env.getProperty("server.port"); 
+		return msg;
 	}
 	
 }
